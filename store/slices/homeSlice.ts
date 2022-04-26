c61afd0dda7929c6c6ad1ac3283b04f0d6130e31
wuyangyi <wuyangyi@haitou.cc>
2022-04-26 16:21:56 +0800

    update

--- store/slices/homeSlice.ts
+++ store/slices/homeSlice.ts
@@ -45,24 +45,27 @@ export const homeSlice = createSlice({
   extraReducers: {
     [fetchRecommendList.fulfilled.type]: (state, action: PayloadAction<Res<Array<HelperItemData>>>) => {
       state.isLoad = true;
-      state.recommendData = action.payload.data;
+      state.recommendData = action.payload.data || [];
       console.log("fetchRecommendList", action.payload.data ? action.payload.data.length : 0);
 
     },
     [fetchLikeList.fulfilled.type]: (state, action: PayloadAction<Res<HelperItemData[]>>) => {
-      state.likeData = action.payload.data;
+      state.likeData = action.payload.data || [];
       console.log("fetchLikeList", action.payload.data ? action.payload.data.length : 0);
     },
     [fetchNearbyList.fulfilled.type]: (state, action: PayloadAction<Res<HelperItemData[]>>) => {
-      state.nearbyData = action.payload.data;
+      state.nearbyData = action.payload.data || [];
       console.log("fetchNearbyList", action.payload.data ? action.payload.data.length : 0);
     },
     [fetchHortList.fulfilled.type]: (state, action: PayloadAction<Res<HelperItemData[]>>) => {
-      state.hortData = action.payload.data;
+      state.hortData = action.payload.data || [];
       console.log("fetchHortList", action.payload.data ? action.payload.data.length : 0);
     },
     [fetchRecommendCircleList.fulfilled.type]: (state, action: PayloadAction<Res<ListPageBean<CircleBean>>>) => {
-      state.recommendCircleData = action.payload.data;
+      state.recommendCircleData = action.payload.data || {
+        total: 0,
+        list: []
+      };
     },
   },
 });
